d7bb45792d46231849c90b6fe24d816225bf5710
JC Garza <jgarza0127@yahoo.com>
2023-05-03 21:04:37 -0500

    Finish Queue

---
+++ Assignment 2/Queue.java
@@ -0,0 +1,52 @@
+public class Queue {
+    public class Node {
+        public int data;
+        public Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    Node head;
+    Node tail;
+
+    public Queue() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // Time Complexity: O(1) - accessing the head which we already have is constant
+    public int peek() {
+        return head != null ? head.data : -1;
+    }
+
+    // Time Complexity: O(1) - inserting is constant since have access to tail
+    public void enqueue(int x) {
+        Node newNode = new Node(x);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            if (tail == null)
+                tail = newNode;
+            else
+                tail.next = newNode;
+        }
+    }
+
+    // Time Complexity: O(1) - remove the head from the LL and return it
+    public int dequeue() {
+        if (head == null)
+            return -1;
+
+        int res = head.data;
+        head = head.next;
+        return res;
+    }
+
+    // Time Complexity: O(1) - just need to check if the head is null or not
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
